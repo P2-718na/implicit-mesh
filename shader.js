adcd28cdc78819d5c01778318fe0cc84f263cef5
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-09-19 16:42:22 -0400

    using all 4 channels

--- shader.js
+++ shader.js
@@ -15,13 +15,20 @@ module.exports = function (size, src) {
     frag: `
       precision mediump float;
       ${src}
-      void main () {
-        float i = gl_FragCoord.x + gl_FragCoord.y * ${st(sq)};
+      float isurface (float i) {
         float x = mod(i,${sx}) / (${sx}-1.0)*2.0-1.0;
         float y = mod(i/${sx},${sy}) / (${sy}-1.0)*2.0-1.0;
         float z = mod(i/${sx}/${sy},${sz}) / (${sz}-1.0)*2.0-1.0;
-        float v = surface(vec3(x,y,z));
-        gl_FragColor = vec4(clamp(v+0.5,0.0,1.0),0,0,1);
+        return clamp(0.5+surface(vec3(x,y,z)),0.0,1.0);
+      }
+      void main () {
+        float i = (gl_FragCoord.x + gl_FragCoord.y * ${st(sq)}) * 4.0;
+        gl_FragColor = vec4(
+          isurface(i+0.0),
+          isurface(i+1.0),
+          isurface(i+2.0),
+          isurface(i+3.0)
+        );
       }
     `,
     vert: `
@@ -50,11 +57,10 @@ module.exports = function (size, src) {
     regl.draw()
     var data = regl.read()
     var ndata = new Float32Array(len)
-    for (var i = 0; i < data.length; i += 4) {
-      var k = i / 4
-      var x = k%size[0]
-      var y = Math.floor(k/size[0])%size[1]
-      var z = Math.floor(k/size[0]/size[1])%size[2]
+    for (var i = 0; i < data.length; i++) {
+      var x = i%size[0]
+      var y = Math.floor(i/size[0])%size[1]
+      var z = Math.floor(i/size[0]/size[1])%size[2]
       var j = x + y*size[0] + z*size[0]*size[1]
       ndata[j] = (data[i] - 127.5) / 127.5
     }
